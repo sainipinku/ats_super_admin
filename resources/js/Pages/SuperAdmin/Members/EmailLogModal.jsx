b61faa1a847bb5748b19fb1fdde132b50a85f8d6
sainipinku <ramchandrasaini63@gmail.com>
2026-05-19 22:51:31 +0530

    push thecode

--- resources/js/Pages/SuperAdmin/Members/EmailLogModal.jsx
+++ resources/js/Pages/SuperAdmin/Members/EmailLogModal.jsx
@@ -3,6 +3,9 @@ import Modal from '@/Components/Modal';
 import Loading from '@/Components/Loading';
 import { Head, router } from '@inertiajs/react';
 
+
+
+
 export default function EmailLogModal({ member, show, onClose }) {
     const [emailLogs, setEmailLogs] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
